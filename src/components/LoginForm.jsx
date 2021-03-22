e41c36393266252378f394995557897b70699503
hemsleek <asiyanbimubashir@gmail.com>
2021-03-22 22:00:33 +0100

    feat: testing app

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
 
 const LoginForm = ({
-    username,
-    password,
-    handleSubmit,
-    handleUsernameChange,
-    handlePasswordChange
+  username,
+  password,
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange
 }) => {
-    return (
-        <div className="LoginForm">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-            <div>
+  return (
+    <div className="LoginForm">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
                 username
-                <input
-                type="text"
-                value={username}
-                name="Username"
-                onChange={ handleUsernameChange }
-                />
-            </div>
-            <div>
-                password
-                <input
-                type="password"
-                value={password}
-                name="Password"
-                onChange={ handlePasswordChange}
-                />
-            </div>
-            <button type="submit">login</button>
-            </form>
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={ handleUsernameChange }
+          />
         </div>
-    )
+        <div>
+                password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={ handlePasswordChange}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
 }
 
 export default LoginForm
