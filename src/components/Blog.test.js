73ed78979577964f52371d8e3c1a2b86d5d7d2bd
hemsleek <asiyanbimubashir@gmail.com>
2021-04-01 12:52:58 +0100

    update: testing blog rendering

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
+test('renders title content and author', () => {
   const blog = {
     title: 'Component testing is done with react-testing-library',
     author: 'Asiyanbi Mubashir',
@@ -18,4 +18,27 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent(
     'Component testing is done with react-testing-library'
   )
+  expect(component.container).toHaveTextContent(
+    'ASIYANBI MUBASHIR'
+  )
+})
+
+test('does not render url and likes ', () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Asiyanbi Mubashir',
+    url:'http:hemsleek.com',
+    likes:2
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+  expect(component.container).not.toHaveTextContent(
+    'Likes - 2'
+  )
+  expect(component.container).not.toHaveTextContent(
+    'http:hemsleek.com'
+  )
+
 })
